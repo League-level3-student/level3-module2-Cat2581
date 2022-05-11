702ca14e76aa11e1c98a4ae2724e26eb0b24ed93
heidi <heidi@HDCPC>
2022-05-10 18:24:58 -0700

    commit

--- src/_00_Intro_to_Sorting_Algorithms/_00_SortingDemo.java
+++ src/_00_Intro_to_Sorting_Algorithms/_00_SortingDemo.java
@@ -1,5 +1,8 @@
 package _00_Intro_to_Sorting_Algorithms;
 
+import java.awt.Color;
+import java.util.Random;
+
 public class _00_SortingDemo {
     public static void (String[] args) {
         /*
@@ -90,10 +93,54 @@ public class _00_SortingDemo {
                 }
             }
         }
+        public static void main(String[] args)
+    	{	String s1 = "Computer Science";
+    		int x = 307;
+    		String s2 = s1 + " " + x;
+    		String s3 = s2.substring(10,17);
+    		String s4 = "is fun";
+    		String s5 = s2 + s4;
+    		
+    		System.out.println("s1: " + s1);
+    		System.out.println("s2: " + s2);
+    		System.out.println("s3: " + s3);
+    		System.out.println("s4: " + s4);
+    		System.out.println("s5: " + s5);
+    		
+    		//showing effect of precedence
+    		
+    		x = 3;
+    		int y = 5;
+    		String s6 = x + y + "total";
+    		String s7 = "total " + x + y;
+    		String s8 = " " + x + y + "total";
+    		System.out.println("s6: " + s6);
+    		System.out.println("s7: " + s7);
+    		System.out.println("s8: " + s8);
+    	}
+
+		Color[][] result = new Color[image.length][image[0].length];
+
+		for(int row = 0; row < image.length; row++)
+		{	for(int col = 0; col < image[0].length; col++)
+			{	result[row][col] = aveOfNeighbors(image, row, col, neighberhoodSize);
+			}
+		}
+
+		return result;
         
         for( String i : arr3 ) {
             System.out.print(i + " ");
         }
         System.out.println();
     }
+    StringBuilder salt = new StringBuilder();
+    Random rnd = new Random();
+    while (salt.length() < 18) { // length of the random string.
+        int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+        salt.append(SALTCHARS.charAt(index));
+    }
+    String saltStr = salt.toString();
+    return saltStr;
+
 }
